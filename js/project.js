698523a0f4abcb8407af42690873dedb9b674127
saheb1592 <sahebsidhu6@gmail.com>
2023-06-30 22:32:57 -0600

    Second commit

--- js/project.js
+++ js/project.js
@@ -2,15 +2,15 @@
     Author: Saheb Sidhu
     This is the JS for Intro to Web Development Project.
 */
-/*
+
 // variables
 
 const apiUrl = "https://api.punkapi.com/v2/beers/random";
-const button = document.getElementById("button");
+const button = document.getElementById("beerButton");
 const rightPanel = document.getElementById("panel-right");
 
 // show data after pressing the button
-document.getElementById("button").addEventListener('click', getBeer);
+button.addEventListener('click', getBeer);
 
 // fetch the data
 async function getBeer (){
@@ -22,50 +22,38 @@ async function getBeer (){
     // for loop to display on the page
     for(const item of data) {
         const beerDiv = document.createElement("div");
-        beerDiv.style.height = "40px";
-        const headName = document.createElement("h3");
+        beerDiv.style.height = "90px";
+        beerDiv.style.width = "70%";
+        beerDiv.style.float = "left";
+        const headName = document.createElement("h1");
+        const tagLine = document.createElement("h3");
         const paraDesc = document.createElement("p");
+        const beerImgDiv = document.createElement("div");
+        beerImgDiv.style.float = "right";
+        beerImgDiv.style.marginRight = "70px";
+        beerImgDiv.style.marginTop = "50px";
         const image = document.createElement('img');
         image.style.height = "400px";
         image.style.width = "200px";
+        const foodPair = document.createElement("p");
+        foodPair.textContent = "Try it with: ";
         const nameElement = item.name;
         const descElement = item.description;
+        const tagLineElement = item.tagline;
+        const foodPairElement = item.food_pairing;
         image.src = item.image_url;
+        tagLine.append(tagLineElement);
+        foodPair.append(foodPairElement);
         headName.append(nameElement);
         paraDesc.append(descElement);
         beerDiv.append(headName);
-        beerDiv.append(paraDesc)
-        beerDiv.append(image);
+        beerDiv.append(tagLine);
+        beerDiv.append(paraDesc);
+        beerDiv.append(foodPair);
+        beerImgDiv.append(image);
         rightPanel.append(beerDiv);
+        rightPanel.append(beerImgDiv);
         rightPanel.style.color = "white";
     }
    });
-}
-*/
-
-// variables and api key
-import apiKey from "./apikey.js";
-const rightPanel = document.getElementById("panel-right");
-const button = document.getElementById("button");
-const apiUrl = "https://api.spoonacular.com/mealplanner/generate";
-
-// How the fetched data should load
-button.addEventListener("click", getMealPlan);
-
-//fetch data
-async function getMealPlan() {
-    return fetch(apiUrl, {
-        method: "GET",
-        headers:{
-            "Content-Type": "application/json",
-            "x-api-key": apiKey
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        rightPanel.innerHTML = "" 
-        for (const item of data) {
-            
-        }
-    })
 }
